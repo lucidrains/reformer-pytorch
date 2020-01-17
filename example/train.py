17d9867b81dfad9a3cac0eea1a069c976847f71d
Phil Wang <lucidrains@gmail.com>
2020-01-17 14:41:12 -0800

    fix numpy split in example

--- example/train.py
+++ example/train.py
@@ -40,7 +40,7 @@ model.cuda()
 
 with gzip.open('./data/enwik8.gz') as file:
     X = np.fromstring(file.read(int(95e6)), dtype=np.uint8)
-    trX, vaX = np.split(X, [int(90e6), int(5e6)])
+    trX, vaX = np.split(X, [int(90e6)])
     data_train, data_val = torch.from_numpy(trX), torch.from_numpy(vaX)
 
 class TextSamplerDataset(Dataset):
