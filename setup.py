c3e1e18c8c12719588ea9c205f968a2868c42a2a
Phil Wang <lucidrains@gmail.com>
2020-04-19 18:10:21 -0700

    add local attention

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'reformer_pytorch',
   packages = find_packages(exclude=['examples', 'pretraining']),
-  version = '0.21.1',
+  version = '0.22.0',
   license='MIT',
   description = 'Reformer, the Efficient Transformer, Pytorch',
   author = 'Phil Wang',
